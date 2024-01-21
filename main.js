c2f26c777f03426a46760707a16f1901903c7b5c
Ameer <142540569+ameerrasras@users.noreply.github.com>
2024-01-21 19:15:50 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+function twoSum(nums, target) {
+    let numMap = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (numMap.has(complement)) {
+            return [numMap.get(complement), i];
+        }
+        numMap.set(nums[i], i);
+    }
+
+    return []; 
+}
+
+
+const nums = [2, 7, 11, 15];
+const target = 9;
+const result = twoSum(nums, target);
+console.log(result); 
